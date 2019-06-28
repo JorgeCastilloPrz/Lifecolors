c1c141c511f0f529100489a7ea618de3881f96e8
Jorge Castillo <jorge.castillo.prz@gmail.com>
2019-06-28 15:46:41 +0200

    first commit

---
+++ app/src/main/java/dev/jorgecastillo/lifecolors/MainActivity.kt
@@ -0,0 +1,12 @@
+package dev.jorgecastillo.lifecolors
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
