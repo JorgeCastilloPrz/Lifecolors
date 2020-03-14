5e5019925fb5133d49824bc896bb07ed66b7c227
Jorge Castillo <jorge.castillo.prz@gmail.com>
2020-03-14 10:51:40 +0100

    Adds view all colors button.

--- app/src/main/java/dev/jorgecastillo/lifecolors/dashboard/presentation/DashboardViewModel.kt
+++ app/src/main/java/dev/jorgecastillo/lifecolors/dashboard/presentation/DashboardViewModel.kt
@@ -1,6 +1,7 @@
 package dev.jorgecastillo.lifecolors.dashboard.presentation
 
 import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.FirebaseAuth
 import dev.jorgecastillo.lifecolors.clothes.domain.ClothingItem
 import dev.jorgecastillo.lifecolors.clothes.domain.ClothingRepository
 import dev.jorgecastillo.lifecolors.colors.network.FirebaseColorsDatabase
@@ -18,42 +19,44 @@ import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 
 data class ContentViewState(
-  val clothes: List<ClothingItem>,
-  val colors: List<ColorViewState>
+    val clothes: List<ClothingItem>,
+    val colors: List<ColorViewState>,
+    val needsLogin: Boolean
 )
 
 @ExperimentalCoroutinesApi
 class DashboardViewModel(
-  private val clothingRepo: ClothingRepository = ClothingRepository(),
-  private val colorsDatabase: FirebaseColorsDatabase = FirebaseColorsDatabase()
+    private val clothingRepo: ClothingRepository = ClothingRepository(),
+    private val colorsDatabase: FirebaseColorsDatabase = FirebaseColorsDatabase()
 ) : BaseViewModel<ContentViewState>() {
 
   fun onReadyToLoadContent(clothingCategory: ZalandoApiClient.ZalandoCategory) {
     viewModelScope.launch(Dispatchers.IO) {
 
       val clothesAndColorsFlow = clothingRepo.getFavedItems(clothingCategory)
-        .combine(colorsDatabase.getFavedColors()) { clothes, colors ->
-          ContentViewState(
-            if (clothes.isEmpty()) ClothingItem.placeholders() else clothes.take(6),
-            if (colors.isEmpty()) {
-              ColorViewState.placeholders()
-            } else {
-              colors.map {
-                ColorViewState(
-                  it,
-                  ColorType.GENERATED, isFavorite = true, isLoading = false
-                )
-              }
-            })
-        }
+          .combine(colorsDatabase.getFavedColors()) { clothes, colors ->
+            ContentViewState(
+                if (clothes.isEmpty()) ClothingItem.placeholders() else clothes.take(6),
+                if (colors.isEmpty()) {
+                  ColorViewState.placeholders()
+                } else {
+                  colors.take(3).map {
+                    ColorViewState(
+                        it,
+                        ColorType.GENERATED, isFavorite = true, isLoading = false
+                    )
+                  }
+                },
+                needsLogin = FirebaseAuth.getInstance().currentUser?.uid == null)
+          }
 
       clothesAndColorsFlow.onEach { state ->
-          updateViewState { ViewState.Content(state) }
-        }
-        .catch {
-          updateViewState { ViewState.Error }
-        }
-        .collect()
+            updateViewState { ViewState.Content(state) }
+          }
+          .catch {
+            updateViewState { ViewState.Error }
+          }
+          .collect()
     }
   }
 }
